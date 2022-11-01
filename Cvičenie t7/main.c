baa1170cc9045a526d96644a04dec077a5072d4b
TobMit <80719513+tobmit@users.noreply.github.com>
2022-11-01 20:00:21 +0100

    Lepšie načítavanie, aj viac znakov

--- Cvičenie t7/main.c
+++ Cvičenie t7/main.c
@@ -3,14 +3,17 @@
 //
 
 #include "stdio.h"
+#include "string.h"
 int main() {
     char name[30+1]; // posledný znak je určite null
     int year;
 
 
     printf("Zadaj meno: ");
-    scanf("%s", name); // polia sa automaticky konvertujú na char* takže preto to tam nemusím dávať
-    printf("Zadaj rok narodenia");
+    //scanf("%s", name); // polia sa automaticky konvertujú na char* takže preto to tam nemusím dávať
+    fgets(name, 30+1, stdin); // tá 30+1 načíta maximálne (30+1)-1 znakov, takže keď chcem načítať 30 znakov tak musím pripočítať ešte 1
+    name[strlen(name) - 1] = '\0'; // keďže fgets načítava aj nový riadk, tak posledný znak ktorý bol načítaný nahradíme za null
+    printf("Zadaj rok narodenia: ");
     scanf("%d", &year);
 
     printf("Ahoj %s, mas %d rokov.\n" , name, 2022-year);
