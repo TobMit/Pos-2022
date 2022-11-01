046e786aa93d29f1d69e087e8bef922dddebc44f
TobMit <80719513+tobmit@users.noreply.github.com>
2022-11-01 19:49:21 +0100

    Už niečo naprogramované

--- Cvičenie t7/main.c
+++ Cvičenie t7/main.c
@@ -4,6 +4,15 @@
 
 #include "stdio.h"
 int main() {
-    printf("Hello world \n");
+    char name[30+1]; // posledný znak je určite null
+    int year;
+
+
+    printf("Zadaj meno: ");
+    scanf("%s", name); // polia sa automaticky konvertujú na char* takže preto to tam nemusím dávať
+    printf("Zadaj rok narodenia");
+    scanf("%d", &year);
+
+    printf("Ahoj %s, mas %d rokov.\n" , name, 2022-year);
     return 0;
 }
