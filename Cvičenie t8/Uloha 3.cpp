f237aaf8c0beee49e4b006b824810856e518df7a
TobMit <80719513+tobmit@users.noreply.github.com>
2022-11-07 11:37:10 +0100

    Excec cez argumenty programu

--- Cvičenie t8/Uloha 3.cpp
+++ Cvičenie t8/Uloha 3.cpp
@@ -2,7 +2,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <cstdio>
-int main() {
+int main(int argc, char * argv[]) {
     printf("Vystvaram potomka, som %d\n", getpid());
     pid_t ret = fork(); // vytvorí child proces ako duplicitu aktuálneho procesu
     // keď poutžívame fork a pokiaľ nepočkáme z predka na potomoka, mohol by vzniknúť zoombie process
@@ -10,8 +10,10 @@ int main() {
     if (ret < 0) {
         perror("nastala chyba pri forku! \n");
     } else if (ret == 0) {
+        //execlp("ls", "ls", "-l", "-a", "/tmp", (char *) NULL); // vypíše nam tmp
         printf("Nachádzam sa v potomkovy, moje id je %d, môj predok ma id %d\n", getpid(), getppid());
-        return 55;
+        execvp(argv[1], argv+1); // pozerám sa na to ako na pole ale začínam od 1 prvku (nie od nultého) // exec prepíše všetko čo sa spúšťa po tom
+        return 55; // táto hodnota sa nám vracia po skončení detského procesu a nájdeme ju v wstatus
     } else {
         printf("Predok sa ozyva po forku, moje pid je %d, môj predok je %d  a môj potomok je %d\n", getpid(), getppid(), ret);
 
@@ -23,7 +25,7 @@ int main() {
             perror("nastala chyba pri ukončeni potomka");
         }
 
-        printf("wstatus. %d\n", wstatus);
+        //printf("wstatus. %d\n", wstatus);
         return 0;
     }
     return 0;
